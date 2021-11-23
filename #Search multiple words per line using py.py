8a5a3a8a645fd56488d8223eed2974b4985390b7
x-cod3r <dr.mah2021@gmail.com>
2021-11-23 20:28:01 +0200

    Update #Search multiple words per line using py.py

--- #Search multiple words per line using py.py
+++ #Search multiple words per line using py.py
@@ -1,11 +1,19 @@
 #Search multiple words per line using python
 import codecs
+#the path for the output file
 file = open('/Users/AboulNasr/Vs/c++/esraa4.txt' , 'w' , encoding="utf8" )
+#the path for the input file
 with open('/Users/AboulNasr/Vs/c++/4.txt' , 'rt' , encoding="utf8") as f:
     data = f.readlines()
 for line in data:
+    #condition one
+    #write the word with capital and small letter to make search case-insensitive
     if line.__contains__('alexandria') or line.__contains__('Alexandria') :
-        if line.__contains__('esraa') or line.__contains__('Esraa') :
+        #condition two
+        #you can make it case sensitive by writing the same word again in capital or small case
+        if line.__contains__('esraa') or line.__contains__('esraa') :
+            #condition three
+            #you can add more conditions by adding more if statements
             if line.__contains__('veterinary') or line.__contains__('Veterinary') :
              file.write( line )
 file.close()
